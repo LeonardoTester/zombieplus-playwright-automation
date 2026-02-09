2796b706ac0cbbf86dfab8d6083314653d0728df
unknown <LeonardoTester>
2026-02-09 18:48:01 -0300

    added new test for register

--- projects/tests/leads.spec.js
+++ projects/tests/leads.spec.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('The user must register a lead' , async ({ page }) => {
+test('deve cadastrar um lead na fila de espera' , async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
   await page.getByRole('button', { name: /Aperte o play/ }).click()
@@ -27,4 +27,55 @@ test('The user must register a lead' , async ({ page }) => {
 
   await expect(page.locator('.toast')).toBeHidden({timeout: 5000})
 
+});
+
+test('não deve cadastrar quando o email é incorreto' , async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  await page.getByRole('button', { name: /Aperte o play/ }).click()
+
+  await expect(page.getByTestId('modal').getByRole('heading')) // checkpoint
+  .toHaveText('Fila de espera')
+
+  await page.getByPlaceholder('Seu nome completo').fill('leo')
+  await page.getByPlaceholder('Seu email principal').fill('leogmailcom')
+
+  await page.getByTestId('modal')
+  .getByText('Quero entrar na fila!').click()
+
+  await expect(page.locator('.alert')).toHaveText('Email incorreto')
+});
+
+test('nao deve casdastrar quando o email não é preenchido' , async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  await page.getByRole('button', { name: /Aperte o play/ }).click()
+
+  await expect(page.getByTestId('modal').getByRole('heading')) // checkpoint
+  .toHaveText('Fila de espera')
+
+  await page.getByPlaceholder('Seu nome completo').fill('leo')
+
+  await page.getByTestId('modal')
+  .getByText('Quero entrar na fila!').click()
+
+  await expect(page.locator('.alert')).toHaveText('Campo obrigatório')
+});
+
+test('nao deve casdastrar quando o nome e email não é preenchido' , async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  await page.getByRole('button', { name: /Aperte o play/ }).click()
+
+  await expect(page.getByTestId('modal').getByRole('heading')) // checkpoint
+  .toHaveText('Fila de espera')
+
+  await page.getByTestId('modal')
+  .getByText('Quero entrar na fila!').click()
+
+  await expect(page.locator('.alert')).toHaveText([
+    'Campo obrigatório',
+    'Campo obrigatório'
+  ])
+  
 });
